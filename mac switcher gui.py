1b2753f6002457d70d51edf0bcdbce6bbd62b4b5
jgrunde <grundmeyerjohn@gmail.com>
2023-08-16 08:59:04 -0500

    Version 0.3

--- mac switcher gui.py
+++ mac switcher gui.py
@@ -1,46 +1,52 @@
 import tkinter as tk
 
-def printing(myString, n):
-    myList = ["", ":", "-"]
-    bigString = ""
-    for i in myList:
-        bigString += "\n" + i.join((myString.lower()).split(n))
-        bigString += "\n" + i.join((myString.upper()).split(n))
-    return bigString
-
-def printingTwo(myString):
-    bigString = ""
+def printing(mac):
     listOne = []
+    listCisco = []
     stringOne = ""
+    stringCisco = ""
+    bigString = ""
     i = 0
     while i < 12:
-        stringOne += myString[i]
+        stringOne += mac[i]
+        stringCisco += mac[i]
         i += 1
         if len(stringOne) == 2:
             listOne.append(stringOne)
             stringOne = ""
+        if len(stringCisco) == 4:
+            listCisco.append(stringCisco)
+            stringCisco = ""
     myList = ["", ":", "-"]
     for i in myList:
-        bigString += "\n" + (i.join(listOne)).lower()
-        bigString += "\n" + (i.join(listOne)).upper()
+        bigString += "\n" + (i.join(listOne))
+        bigString += "\n" + ((i.join(listOne)).upper())
+    bigString += "\n" + (".".join(listCisco))
+    bigString += "\n" + ((".".join(listCisco)).upper())
     return bigString
 
 def returnText(enteredMac):
+    mac = enteredMac.strip() 
     endGoal = ""
-    if (":" in enteredMac):
-        endGoal = printing(enteredMac, ":")
-    elif ("-" in enteredMac):
-        endGoal = printing(enteredMac, "-")
-    elif (len(enteredMac) == 12):
-        endGoal = printingTwo(enteredMac)
+
+    if (":" in mac):
+        endGoal = printing("".join(mac.split(":")))
+    elif ("-" in mac):
+        endGoal = printing("".join(mac.split("-")))
+    elif ("." in mac):
+        endGoal = printing("".join(mac.split(".")))   
+    elif (len(mac) == 12):
+        endGoal = printing(mac)
     return endGoal
 
 def handle_keypress(event, entry_window, result_label):
     mac = entry_window.get()
-    result_label.insert(1.0, mac + "\n" + returnText(mac))
+    result_label.delete(1.0, tk.END)
+    result_label.insert(1.0, returnText(mac))
 
 def main():
     window = tk.Tk()
+    window.title("MAC Address Converter")
     greeting = tk.Label(text="Enter a MAC")
     entry = tk.Entry()
     result = tk.Text()
